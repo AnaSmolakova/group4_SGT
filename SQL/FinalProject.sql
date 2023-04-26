6cbb3e871839b5eea822ab4989bb28f69f6ca485
Viktorija Lietuvnike <v.aurylaite@gmail.com>
2023-04-26 18:26:38 +0300

    test

--- SQL/FinalProject.sql
+++ SQL/FinalProject.sql
@@ -1,17 +1,128 @@
-CREATE TABLE login(
-sid int NOT NULL PRIMARY KEY,
-name text NOT NULL
+CREATE TABLE psychologytest (
+questionID int (45) NOT NULL auto_increment,
+section int (45) NOT NULL,
+question varchar (400) NOT NULL,
+PRIMARY KEY (questionID)
 );
 
-CREATE TABLE questions(
-tid int NOT NULL PRIMARY KEY,
-name text NOT NULL
-);
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am detailed');
 
-CREATE TABLE results (
-loginInfo int NOT NULL REFERENCES usersName(login),
-questionsInfo int NOT NULL REFERENCES question(qustions),
-answerUser int NOT NULL REFERENCES answer(questions),
-answerUser int NOT NULL,
-PRIMARY KEY (studentID, teacherID, subjectID)
-);
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am consistent');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am reserved');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am practical');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am factual');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am perfectionist');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am enjoys instructions');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am inquisitive');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am persistent');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am sensitive');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am accurate');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am controlled');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am predictable');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am orderly');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am conscientious');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am discerning');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am analytical');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am precise');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am organised');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('3', 'I am deliberate');
+
+
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am sensitive');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am calm');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am easy-going');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am avoids confrontation');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am enjoys routine');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am warm');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am adaptable');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am thoughtful');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am patient');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am good listener');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am loyal');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am well-balanced');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am gives in easily');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am indecisive');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am dislikes changes');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am dry humor');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am sympathetic');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am nurturing');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am tolerant');
+
+INSERT INTO psychologytest (section, question)
+VALUES ('4', 'I am peace maker');
