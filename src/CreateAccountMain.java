6bbb372326218dde05d9e3180f304d608ebf8016
Marina Dzjubenko <kojate.marina@gmail.com>
2023-04-27 11:38:53 +0300

    loginInfo

--- src/CreateAccountMain.java
+++ src/CreateAccountMain.java
@@ -1,47 +1,75 @@
+package project;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CreateAccountMain {
-
     public static void main(String[] args){
-        Pattern pattern = Pattern.compile("abc");
-        Matcher matcher = pattern.matcher("abc");
-
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Please, create new user name, which consists of 3 uppercase letter and at least 3 numbers. ");
-        String newUserName = scanner.nextLine();
-        if (Pattern.matches("[A-Z]{3}[1-9]{3,20}", newUserName)) {
-            System.out.println("Your inputted user name is valid");
-        } else {
-            System.out.println("Your inputted user name is not valid, please try one more time.");
+        String expectedUserName = "[A-Z]{3}[1-9]{3,20}";
+        Pattern UserNamepattern = Pattern.compile(expectedUserName);
+        String enteredUserName;
+        Matcher matcher;
+        do {
+            System.out.println("Please, create new user name, which consists of 3 uppercase letter and at least 3 numbers. ");
+            enteredUserName = scanner.nextLine();
+            matcher = UserNamepattern.matcher(enteredUserName);
+            if (!matcher.matches()) {
+                System.out.println("Invalid input. Please try again.");
         }
+        } while (!matcher.matches());
+        System.out.println("Input accepted.");
 
-        System.out.println("Please, create new password, which consists of 4 numbers.");
-        String newUserPassword = scanner.nextLine();
-        if (Pattern.matches("[1-9]{4,20}", newUserPassword)) {
-            System.out.println("Your inputted password is valid");
-        } else {
-            System.out.println("Your inputted password is not valid, please try one more time.");
-        }
+        String expectedPassword = "[1-9]{4,20}";
+        Pattern Passwordpattern = Pattern.compile(expectedPassword);
+        String enteredPassword;
+        Matcher Passwordmatcher;
+        do {
+            System.out.println("Please, create new password, which consists of 4 numbers.");
+            enteredPassword = scanner.nextLine();
+            matcher = Passwordpattern.matcher(enteredPassword);
+            if (!matcher.matches()) {
+                System.out.println("Invalid input. Please try again.");
+            }
+        } while (!matcher.matches());
+        System.out.println("Input accepted.");
 
-        System.out.println("Please, enter Your full name.");
-        String newUserFullName = scanner.nextLine();
-        if (Pattern.matches("[a-zA-Z]{1,20}+(\s)+[a-zA-Z]{1,20}", newUserFullName)) {
-            System.out.println("Your inputted full name is valid");
-        } else {
-            System.out.println("Your inputted full name is not valid, please try one more time. ");
-        }
+        String expectedFullName = "[a-zA-Z]{1,20}+(\s)+[a-zA-Z]{1,20}";
+        Pattern FullNamepattern = Pattern.compile(expectedFullName);
+        String enteredFullName;
+        Matcher FullNamematcher;
+        do {
+            System.out.println("Please, enter Your full name.");
+            enteredFullName = scanner.nextLine();
+            matcher = FullNamepattern.matcher(enteredFullName);
+            if (!matcher.matches()) {
+                System.out.println("Invalid input. Please try again. ");
+            }
+        } while (!matcher.matches());
+        System.out.println("Input accepted.");
+
+        String expectedEmail = "^[A-Za-z0-9+_.-]+@(.+)$";
+        Pattern Emailpattern = Pattern.compile(expectedEmail);
+        String enteredEmail;
+        Matcher Emailmatcher;
+
+        do {
+            System.out.println("Please, enter Your email.");
+            enteredEmail = scanner.nextLine();
+            matcher = Emailpattern.matcher(enteredEmail);
+            if (!matcher.matches()) {
+                System.out.println("Your inputted email is not valid, please try one more time. ");
+            }
+        } while (!matcher.matches());
+        System.out.println("Input accepted.");
+
 
         System.out.println("Please, enter Your email.");
         String newEmail = scanner.nextLine();
         if (Pattern.matches("^[A-Za-z0-9+_.-]+@(.+)$", newEmail)) {
             System.out.println("Your inputted email is valid");
         } else {
-            System.out.println("Your inputted email is not valid, please try one more time. ");
+            System.out.println("Invalid input. Please try again. ");
         }
     }
 }
-
-
